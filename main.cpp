78e8faba6522367aa13837db8fa4cccd95aafb30
jesus <jesusmejded@gmail.com>
2016-11-17 18:21:57 -0600

    Iniciando salida

--- main.cpp
+++ main.cpp
@@ -16,12 +16,13 @@ int max(int a, int b){
 }
 
 int* output;
+int output_index = 0;
 
 
 int alpha_beta(Node* tree, int actual, int alpha, int beta, bool maxx){
 
 	if (tree[actual].getChildren() == NULL) {
-		cout << "\n >>> " << "  " << actual << "  " << tree[actual].getValue() << endl;
+		output[output_index++] = actual;
 		return tree[actual].getValue();
 	}
 
@@ -61,13 +62,13 @@ int main(int argc, char const *argv[])
 
 	int data_len, branches, i = 0, tree_len = 0;
 
-	cout << "Cantidad de ramas por nodo: ";
+	// cout << "Cantidad de ramas por nodo: ";
 	cin >> branches;
 
-	cout << "Cantidad de datos: ";
+	// cout << "Cantidad de datos: ";
 	cin >> data_len;
 
-	cout << "Introduce los valores:";
+	// cout << "Introduce los valores:";
 
 	int data[data_len], tmp = data_len;
 
@@ -76,20 +77,38 @@ int main(int argc, char const *argv[])
 			++tree_len;
 
 	Node tree[tree_len];
+	output = new int[data_len];
+
+	for(i = 0; i < data_len;) output[i++] = -1;
 
 	createTree(tree_len, tree, branches);
 
 	// Lectura del vector de datos
 	i = tree_len - data_len;
 	while (i < tree_len){	
-		cout << endl << i << ": "; 
+		// cout << endl << i << ": "; 
 		cin >> tmp; 
 		tree[i++].setValue(tmp);
 	}
 
-	cout << "\nEl árbol se creó con éxito" << endl;
+	// cout << "\nEl árbol se creó con éxito" << endl;
 
-	int value = alpha_beta(tree, 0, -INF, INF, true) << endl;
+	int winner = alpha_beta(tree, 0, -INF, INF, true);
+
+	i = 0;
+	for (int j = 0; j < data_len; ++j) {
+
+		// if (output[i] == choice){
+		// 	cout <<  "(" << tree[output[i++]].getValue() << "), ";
+		// }
+
+		if (output[i] == j + tree_len - data_len )
+			cout << "\033[1m" << tree[output[i++]].getValue() << "\033[0m, ";
+		else
+			cout << "*, ";
+	}
+
+	cout << endl;
 
 
 	return 0;
